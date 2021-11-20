d46a5f01a5b0b0b592208addd7e1e9094b9455a8
Kunji Meena <kunji.m@myparkplus.com>
2021-11-20 14:26:04 +0530

    .

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const isEven = (number) => {
-  return number % 2 === 0
-}
-  
-module.exports = isEven
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+)
